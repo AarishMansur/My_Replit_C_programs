714bc8b1f9b83cc66c4805f9213425d82fb6eafa
AarishMansur <aarishmansur@gmail.com>
2024-12-02 23:45:20 +0530

    Create Array_frequency_number.c

---
+++ Array_frequency_number.c
@@ -0,0 +1,15 @@
+#inclue<stdio.h>
+int main(){
+  int num[] = {1,3,5,6,8,9,9,9,4,6,5,4,};
+  int n,i,count;
+  printf("Enter An element to search :");
+  scanf("%d",&n);
+  count = 0;
+  for(i=0;i<=12;i++){
+    if(num[i]==n){
+      count++;
+    }
+  }
+  printf("Number = %d is found %d times in the arrays",n,count);
+  return 0 ;
+}
