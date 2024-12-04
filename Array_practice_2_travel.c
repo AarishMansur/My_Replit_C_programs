a65451844e6c70501c9390af0dad7165c348a951
AarishMansur <aarishmansur@gmail.com>
2024-12-04 16:55:41 +0530

    Create Array_practice_2_travel.c

---
+++ Array_practice_2_travel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int a[10],i;
+  printf("Enter the elements of arrays.");
+  for(i=0;i<10;i++){
+    scanf("%d",&a[i]);
+  }
+  // Mistake I did : I intitialise the loop from 10 instead of 9 because iteration of arrays start from n-1 not from last term 
+  for(i=9;i>=0;i++){
+    printf("%d",arr[i]);
+  }
+  return 0;
+}
